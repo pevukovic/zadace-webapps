c9e1b4a744e42e69f25535172f80d48f28d30750
Petar Vukovic <pvukovic1111@gmail.com>
2022-10-11 01:39:21 +0200

    Rješen zadatak 2

---
+++ zadatak2.js
@@ -0,0 +1,8 @@
+function provjera(x){
+    if (x >= 100 && x <= 150000){
+    console.log(x + " je unutar [100,150000]")
+} else {
+    console.log(x + " nije unutar [100,150000]")
+}}
+provjera(53)
+
